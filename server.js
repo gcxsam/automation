bdeb204ce5535ae7e6e0b86f15bcb1a189367b3c
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2023-04-23 00:03:58 +0500

    linkedIn added

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ async function twitter() {
       messages: [
         {
           role: "system",
-          content: "You are ChatGPT, a large language model trained by OpenAI.",
+          content: "You are ChatGPT, a large language model trained by OpenAI. Answer concisely",
         },
         // { role: "user", content: "Write a very short tweet with no more than 30 words for the this description: Our platform will offer fractional ownership in high-value real estate properties through the issuance of NFTs. The NFTs will represent a portion of the property's value and will be tradable on our platform. Investors can purchase these NFTs using our utility tokens, which will be ERC-20 compliant and built on the Ethereum blockchain. The utility tokens will provide investors with access to the platform's services and functionalities, including purchasing, trading, and managing their NFT investments. The platform will generate revenue by charging a transaction fee for each NFT purchase or sale. Posts should provoke engagement, and Add human tales the personal element that engenders trust. Also Include hashtag and Emojis. Number of words should be less than 30 words" },
         // {
@@ -47,7 +47,8 @@ async function twitter() {
         // },
         {
           role: "user",
-          content:"Generate tweet of less than 35 words with source, hashtag and emojis using content from this following url: https://www.glocomx.com/nft-adoption-survey"
+          content:
+            "Generate tweet of less than 35 words with source, hashtag and emojis using content from this following url: https://www.glocomx.com/nft-adoption-survey",
         },
       ],
     });
@@ -65,7 +66,7 @@ async function twitter() {
 
       const tweet = async () => {
         try {
-          await twitterClient.v2.tweet(result+"#glocomx #nfts #blockchain");
+          await twitterClient.v2.tweet(result + "#glocomx #nfts #blockchain");
         } catch (e) {
           console.log(e);
         }
@@ -78,8 +79,78 @@ async function twitter() {
   }
 }
 
-cron.schedule("*/15 * * * *", () => {
+async function linkedin() {
+  try {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    // const response = await openai.createCompletion({
+    //   model: "text-davinci-003",
+    //   prompt: " Generate me a twitter post for the topic: Bitcoin. Also Include hashtag and Emojis. Number of words should be between 20 - 40 words",
+    //   temperature: 0,
+    //   max_tokens: 2048,
+    //   top_p: 1,
+    //   frequency_penalty: 0.5,
+    //   presence_penalty: 0,
+    // });
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "You are ChatGPT, a large language model trained by OpenAI. Answer in detail",
+        },
+        {
+          role: "user",
+          content:
+            "Generate a post with source, hashtag and emojis using the following url: https://www.glocomx.com/nft-adoption-survey",
+        },
+      ],
+    });
+    const result = response.data.choices[0].message.content;
+    console.log(result);
+    if (result) {
+      const postData = {
+        author: `urn:li:person:${process.env.PERSON_ID}`,
+        lifecycleState: "PUBLISHED",
+        specificContent: {
+          "com.linkedin.ugc.ShareContent": {
+            shareCommentary: {
+              text: result,
+            },
+            shareMediaCategory: "NONE",
+          },
+        },
+        visibility: {
+          "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC",
+        },
+      };
+
+      axios
+        .post("https://api.linkedin.com/v2/ugcPosts", postData, {
+          headers: {
+            Authorization: `Bearer ${process.env.LINKEDIN_ACCESS_TOKEN}`,
+            "Content-Type": "application/json",
+            "X-Restli-Protocol-Version": "2.0.0",
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error(error.response.data);
+        });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+cron.schedule("*/2 * * * *", () => {
   twitter();
+  linkedin();
 });
 
 app.post("/linkedin", async (req, res) => {
